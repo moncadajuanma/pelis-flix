b603dec7e5d8599681bb38cf398218b5246abcf8
unknown <jumamon7@gmail.com>
2024-04-04 23:43:32 -0500

    Ajuste boton eliminarCaratula

--- aluraFlix.js
+++ aluraFlix.js
@@ -7,7 +7,6 @@ function añadirPelícula() {
     var trailerPeliculasFavoritas = document.getElementById('trailer').value;
 
     if ((peliculaFavorita.endsWith('jpg')) || (peliculaFavorita.endsWith('jpeg'))) {
-        document.getElementById('mensajeDeError').innerHTML = '';
         listaPeliculasFavoritas.push(peliculaFavorita);
         listaTrailersFavoritas.push(trailerPeliculasFavoritas);
 
@@ -26,14 +25,15 @@ function añadirPelícula() {
         limpiarCampos();
     }
 }
-//Dirección de imagen no válida, inténtalo de nuevo
+
 function recargarPeliculas() {
     console.log(listaPeliculasFavoritas);
     var elementoListaPeliculas = document.getElementById('listaPeliculas');
     elementoListaPeliculas.innerHTML = '';
-    for (i = 0; i < listaPeliculasFavoritas.length; i++) {
+    for (var i = 0; i < listaPeliculasFavoritas.length; i++) {
         elementoListaPeliculas.innerHTML += `<div class="contenedorImg">
-                                             <a target="_blank"  href=" ${listaTrailersFavoritas[i]} "><img src=" ${listaPeliculasFavoritas[i]} "></a>
+                                             <a target="_blank"  href=" ${listaTrailersFavoritas[i]} "> <img src=" ${listaPeliculasFavoritas[i]} "></a>
+                                             <button type= button id= "eliminar">Eliminar</button>
                                              </div>`;
     }
 }
@@ -48,12 +48,18 @@ function storageListas() {
     storage.setItem('listaTrailer', JSON.stringify(listaTrailersFavoritas));
 }
 
+function eliminarCaratula() {
+    storage.removeItem(listaTrailersFavoritas[i]);
+    storage.removeItem(listaPeliculasFavoritas[i]);
+}
+
 let button = document.getElementById("button");
+let buttonEliminar = document.getElementById("eliminar");
 button.addEventListener("click", añadirPelícula);
+//buttonEliminar.addEventListener("click", eliminarCaratula);
 
 // Carga Automatica de variables almacenadas en localStorage
 document.addEventListener("DOMContentLoaded", () => {
-
     if (!(storage.getItem("listaCaratula")) && !(storage.getItem("listaTrailer"))) {
         return;
     }
